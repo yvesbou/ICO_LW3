e3884759b9c26d379ca4d209e951fb83c6b52353
yvesbou <63062257+yvesbou@users.noreply.github.com>
2022-10-03 09:59:36 +0200

    added interface for nft collection

---
+++ contract/src/CryptoDevToken.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.16;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./ICryptoDevs.sol";
+
+contract CryptoDevToken is ERC20, Ownable {
+    uint256 public constant tokenPrice = 0.001 ether;
+    uint256 public constant tokensPerNFT = 10 * 10**18;
+    uint256 public constant maxTotalSupply = 10000 * 10**18;
+
+    ICryptoDevs cryptoDevs;
+
+    constructor(string memory tokenname, string memory symbol)
+        ERC20(tokenname, symbol)
+    {}
+}
