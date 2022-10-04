8b98d3300b1eb0a701e81791b0e668b2452f150e
yvesbou <63062257+yvesbou@users.noreply.github.com>
2022-10-04 06:00:56 +0200

    claim and mint written

--- contract/src/CryptoDevToken.sol
+++ contract/src/CryptoDevToken.sol
@@ -12,7 +12,54 @@ contract CryptoDevToken is ERC20, Ownable {
 
     ICryptoDevs cryptoDevs;
 
-    constructor(string memory tokenname, string memory symbol)
-        ERC20(tokenname, symbol)
-    {}
+    mapping(uint256 => bool) public tokenIdsClaimed;
+
+    constructor(
+        string memory tokenname,
+        string memory symbol,
+        address cryptoDevsAddress
+    ) ERC20(tokenname, symbol) {
+        cryptoDevs = ICryptoDevs(cryptoDevsAddress);
+    }
+
+    function claim() external {
+        uint256 nftsOwned = cryptoDevs.balanceOf(msg.sender);
+        require(nftsOwned > 0, "You need a CryptoDev NFT to Claim Tokens");
+
+        uint256 nftsUnclaimed = 0;
+
+        for (uint256 index = 0; index < nftsOwned; index++) {
+            uint256 tokenId = cryptoDevs.tokenOfOwnerByIndex(msg.sender, index);
+            if (!tokenIdsClaimed[tokenId]) {
+                nftsUnclaimed += 1;
+                tokenIdsClaimed[tokenId] = true;
+            }
+        }
+
+        require(nftsUnclaimed > 0, "You have claimed all your NFTs");
+
+        uint256 amountToClaim = tokensPerNFT * nftsUnclaimed;
+        uint256 currentSupply = totalSupply();
+        require(
+            currentSupply + amountToClaim < maxTotalSupply,
+            "The maximal supply of the token is 10,000"
+        );
+
+        _mint(msg.sender, amountToClaim);
+    }
+
+    function mint(uint256 amount) external payable {
+        require(amount > 0, "Amount needs to be greater than 0");
+
+        require(
+            tokenPrice * amount >= msg.value,
+            "Ether amount for minting tokens is not enough"
+        );
+        uint256 amountWithDecimals = amount * 10**18;
+        require(
+            (totalSupply() + amountWithDecimals) <= maxTotalSupply,
+            "Exceeds the max total supply available."
+        );
+        _mint(msg.sender, amountWithDecimals);
+    }
 }
