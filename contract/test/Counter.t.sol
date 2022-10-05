6807380cc6836a47f51842156a5bb2f9dee74920
yvesbou <63062257+yvesbou@users.noreply.github.com>
2022-10-05 06:17:12 +0200

    setup test structure

--- contract/test/Counter.t.sol
+++ contract/test/Counter.t.sol
@@ -2,22 +2,41 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import "../src/CryptoDevToken.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract CryptoDevTokenTest is Test {
+    CryptoDevToken public cryptoDevToken;
+    address cryptoDevNFTCollection = 0x96788D3aA03B6afAE42F15c059934ac53094Aca8;
+
+    address[2] nftOwners = [
+        0x26a58e69f3FF059191d5a72764eD795779Cb1221,
+        0x23E5BBFD0A97b93EC889C097A3d9C581391603Da
+    ];
+
+    uint256 goerliFork;
+
+    address deployerAndOwner;
+
+    /**
+     * @dev receive() and fallback() are needed if the test contract is the owner
+     * of the deployed token contract, because otherwise withdraw() does not work
+     * because every contract needs to have those functions in order to receive ether
+     */
+
+    // Function to receive Ether. msg.data must be empty
+    receive() external payable {}
+
+    // Fallback function is called when msg.data is not empty
+    fallback() external payable {}
+
     function setUp() public {
-       counter = new Counter();
-       counter.setNumber(0);
-    }
-
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
-
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+        string memory goerli_RPC_URL = vm.envString("GOERLI_RPC_URL");
+        goerliFork = vm.createSelectFork(goerli_RPC_URL);
+        deployerAndOwner = address(this);
+        cryptoDevToken = new CryptoDevToken(
+            "CryptoDev Token",
+            "CD",
+            cryptoDevNFTCollection
+        );
     }
 }
